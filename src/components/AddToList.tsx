24c4df0f854618e717f5f4d77c7e3650bce2e837
Ko-de-Koning <110471206+Ko-de-Koning@users.noreply.github.com>
2022-10-20 17:03:25 +0200

    AddToList input fields typed

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -9,30 +9,54 @@ const AddToList = () => {
         img: ""
     })
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+        setInvoer({
+            ...invoer,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = (): void => {
+    
+    }
+
     return (
         <div className='AddToList'>
             <input
                 type="text"
                 placeholder="Name"
                 className="AddToList-input" 
-                value={invoer.name}       
+                value={invoer.name}
+                onChange={(handleChange)}       
+                name="name"
             />
             <input
                 type="text"
                 placeholder="Age"
                 className="AddToList-input"
-                value={invoer.age}       
+                value={invoer.age} 
+                onChange={handleChange}       
+                name="age"
             />
             <input
                 type="text"
                 placeholder="Image Url"
                 className="AddToList-input" 
-                value={invoer.img}       
+                value={invoer.img} 
+                onChange={handleChange}       
+                name="img"
+
             />
             <textarea
                 placeholder="Notes"
                 className="AddToList-input"
-                value={invoer.note}        
+                value={invoer.note}  
+                name="note"
+                onChange={handleChange}       
+            />
+            <button
+                className='AddToList-btn'
+                onClick={handleClick}
             />
         </div>
     )
